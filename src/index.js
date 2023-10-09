f31ff48bf972f329d8bc98fff678a7be0bb8671f
Mambet Aibar <109662937+Aibar77@users.noreply.github.com>
2023-10-09 20:39:26 +0600

    feat: solve a problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) return Array.from(String(-n)).reverse().join("");
+    return Array.from(String(n)).reverse().join("");
+};
